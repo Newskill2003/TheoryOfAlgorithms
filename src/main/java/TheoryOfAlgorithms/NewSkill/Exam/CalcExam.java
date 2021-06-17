0b6c276f920be3d62c83c92ffef589cbb342619d
EneRGy-79 <skril.iwan@gmail.com>
2021-06-17 14:53:19 +0300

    exam done

---
+++ src/main/java/TheoryOfAlgorithms/NewSkill/Exam/CalcExam.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package TheoryOfAlgorithms.NewSkill.Exam;
+
+public class CalcExam {
+    
+    public static double Calculate(double x)
+    {
+        return 5/(3*x-3);
+    }
+}
